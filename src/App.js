0cf8a84e7c44d3ae3901aa30c9f00f3905e006ea
walter-morawa <33468837+walter-morawa@users.noreply.github.com>
2024-07-23 18:28:05 -0400

    disable scrollbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-// src/App.js
-import React from 'react';
-import Map from './Map';
+import * as React from 'react';
+import Map from 'react-map-gl';
 
 function App() {
   return (
-    <div>
-      <h1>DJI Log Viewer</h1>
-      <Map />
+    <div id='map' style={{ width: '100vw', height: '100vh'}}> 
+    <Map
+      mapboxAccessToken="pk.eyJ1Ijoid2FsdGVyLW1vcmF3YSIsImEiOiJjanBwd3kxajMwdDdoM3dzNGhpcXBja3JmIn0.w0EN49Ij81yTIB8scT_1cg"
+      initialViewState={{
+        longitude: -122.4,
+        latitude: 37.8,
+        zoom: 14
+      }}
+      style={{width: '100%', height: '100%'}}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+      
+    />
     </div>
   );
 }
-
-export default App;
+export default App
