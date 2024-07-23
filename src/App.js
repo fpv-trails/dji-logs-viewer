ca5c2f032f1319abc829307ba462ce1bf3d5169b
walter-morawa <33468837+walter-morawa@users.noreply.github.com>
2024-07-23 16:23:02 -0400

    initial commit - create-react-app
    yes i know its deprecated

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import Map from './Map';
+
+function App() {
+  return (
+    <div>
+      <h1>My Mapbox App</h1>
+      <Map />
+    </div>
+  );
+}
+
+export default App;
